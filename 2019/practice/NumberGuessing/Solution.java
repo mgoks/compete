b993285de689d051de8367be72213cbfd672fcc7
Murat Göksel <murat.goxel@gmail.com>
2020-01-18 23:54:02 +0300

    Clean up code

--- 2019/practice/NumberGuessing/Solution.java
+++ 2019/practice/NumberGuessing/Solution.java
@@ -1,29 +1,26 @@
 import java.util.Scanner;
 
 public class Solution {
-  String guessNumber(Scanner scan, int lowerBound, int upperBound) {
-    int mid = (lowerBound + upperBound)/2;  // both exclusive
+  static void guessNumber(Scanner scan, int low, int high) {
+    int mid = (low + high)/2;
     System.out.println(mid);
-    String response = scan.nextLine();
-    if (response.equals("TOO_SMALL"))
-      return guessNumber(scan, mid, upperBound);
+    String response = scan.next();
+    if (response.equals("TOO_SMALL")) 
+      guessNumber(scan, mid+1, high);
     else if (response.equals("TOO_BIG"))
-      return guessNumber(scan, lowerBound, mid);
-    else
-      return response;
+      guessNumber(scan, low, mid-1);
+    else 
+      return;
   }
-
+  
   public static void main(String[] args) {
     Scanner scan = new Scanner(System.in);
-    Solution guesser = new Solution();
-    int t = Integer.parseInt(scan.nextLine()); // number of test cases
+    int t = scan.nextInt();
     while (t-- > 0) {
-      int lowerBound = scan.nextInt(); // exclusive
-      int upperBound = scan.nextInt(); // inclusive 
-      scan.nextLine();
-      int n = Integer.parseInt(scan.nextLine()); // max number of guesses
-      if (!guesser.guessNumber(scan, lowerBound, upperBound+1).equals("CORRECT"))
-        return; // something went wrong
+      int a = scan.nextInt(); // exlusive
+      int b = scan.nextInt(); // inclusive
+      scan.next(); // max number of attempts redundant
+      guessNumber(scan, a+1, b);
     }
   }
 }
