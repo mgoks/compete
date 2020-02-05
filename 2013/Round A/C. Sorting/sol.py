5728270c1387f3551472c1ae9d1d40d5d59c0de1
Murat Göksel <murat.goxel@gmail.com>
2020-02-05 16:42:41 -0500

    Minor refactoring

--- 2013/Round A/C. Sorting/sol.py
+++ 2013/Round A/C. Sorting/sol.py
@@ -3,23 +3,21 @@ def is_odd(n):
     
 T = int(input())
 for x in range(1, T+1):
-    _ = input() # don't need number of books
-    a, b, o = [], [], []    # Alex, Bob, ordered
-    for w in map(int, input().split()):
-        if is_odd(w):
-            a.append(w)
-            o.append('a')
-        else:
-            b.append(w)
-            o.append('b')
+    _ = input()     # don't need number of books
+    R = [int(s) for s in input().split()]   # random order
+    a, b = [], []   # Alex, Bob
+    for w in R:
+        l = a if is_odd(w) else b
+        l.append(w)
     a.sort()
     b.sort(reverse=True)
     i, j = 0, 0
-    for k in range(len(o)):
-        if o[k] == 'a':
-            o[k] = str(a[i])
+    o = []  # ordered
+    for w in R:
+        if is_odd(w):
+            o.append(str(a[i]))
             i += 1
         else:
-            o[k] = str(b[j])
+            o.append(str(b[j]))
             j += 1
     print("Case #{}: {}".format(x, ' '.join(o)))
