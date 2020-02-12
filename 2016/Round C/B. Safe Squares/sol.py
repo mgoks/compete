84f1ea8b7bd1be15291f92c67d473118726585c1
Halil Murat Göksel <34871949+mgoks@users.noreply.github.com>
2020-02-12 00:20:20 -0500

    does not work

--- 2016/Round C/B. Safe Squares/sol.py
+++ 2016/Round C/B. Safe Squares/sol.py
@@ -1,23 +1,20 @@
 # DRAFT
 
+def has_monster(grid, edge_len, row, col):
+    for r in range(edge_len):
+        for c in range(edge_len):
+            if grid[row+r][col+c] == 1:
+                return False
+    return True
+
 for t in range(1, int(input()) + 1):
     R, C, K = map(int, input().split())
     M = {tuple(map(int, input().split())) for _ in range(K)}  # monster locations
     G = [[1 if (r,c) in M else 0 for c in range(C)] for r in range(R)]   
-    ss = 0  # number of safe squares
-
-    # to get 1x1 squares
-    for r in range(len(G)):
-        for c in range(len(G[r])):
-            if G[r][c] == 0:
-                ss += 1
-    # to get 2x2 squares
-    for upper_row in range(len(G) - 1):
-        for left_col in range(len(G[upper_row]) - 1):
-            monster = False
-            for r in range(2):
-                for c in range(2):
-                    if G[upper_row+r][left+col+c] == 1:
-                        monster = True
-            if not monster:
-                ss += 1
+    n = 0  # number of safe squares
+    for a in range(1, min(R,C)+1):  # square edge length
+        for ur in range(len(G) - a + 1): # upper row
+            for lc in range(len(G[ur]) - a + 1): # left column
+                if not has_monster(G, a, ur, lc):
+                    n += 1
+    print('Case #{}: {}'.format(t, n))
