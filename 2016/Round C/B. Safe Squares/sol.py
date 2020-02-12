2435c02b242e73650dec04f7630e9f912e7fe054
Murat Göksel <murat.goxel@gmail.com>
2020-02-12 12:27:27 -0500

    Solve for small dataset

--- 2016/Round C/B. Safe Squares/sol.py
+++ 2016/Round C/B. Safe Squares/sol.py
@@ -1,12 +1,18 @@
-# DRAFT
-
 def has_monster(grid, edge_len, row, col):
     for r in range(edge_len):
         for c in range(edge_len):
             if grid[row+r][col+c] == 1:
-                return False
-    return True
+                return True
+    return False
 
+def print_submatrix(grid, edge_len, row, col):
+    for r in range(edge_len):
+        for c in range(edge_len):
+            print(str(grid[row+r][col+c]), end='')
+        print('')
+    print('')
+
+# small data set
 for t in range(1, int(input()) + 1):
     R, C, K = map(int, input().split())
     M = {tuple(map(int, input().split())) for _ in range(K)}  # monster locations
@@ -15,6 +21,7 @@ for t in range(1, int(input()) + 1):
     for a in range(1, min(R,C)+1):  # square edge length
         for ur in range(len(G) - a + 1): # upper row
             for lc in range(len(G[ur]) - a + 1): # left column
+                print_submatrix(G, a, ur, lc)
                 if not has_monster(G, a, ur, lc):
                     n += 1
     print('Case #{}: {}'.format(t, n))
